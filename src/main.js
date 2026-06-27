6f85697dcc0ad0eec747f9b84db4c64ce86a1f8a
Daaor <praisedaaor@gmail.com>
2026-06-27 16:30:42 +0100

    revamped js logic to make the search bar functionality work

--- src/main.js
+++ src/main.js
@@ -1,31 +1,41 @@
-
 const url = 'https://jsonplaceholder.typicode.com/users';
 const loading = document.getElementById('loading');
 const allUserData = document.getElementById("allUserId");
+const searchBar = document.getElementById('search');
+let masterList = [];
+
+searchBar.addEventListener("input", (e) => {
+  const searchValue = e.target.value.toLowerCase();
+  const filtered = masterList.filter((user) => { //appends element that passes condition written in its arrow function
+    return user.name.toLowerCase().includes(searchValue);
+  });
+  renderUsers(filtered);
+});
+
+function renderUsers(usersList){ // Handles creating of individual cards from the elements in the array passed as an argument
+  allUserData.innerHTML = '';
+  usersList.forEach((user) => {
+    const userCard = document.createElement('div');
+    userCard.innerHTML = `
+      <h2 class="text-xl font-bold text-center">${user.name}</h2>
+      <p class="text-lg text-center pt-5">${user.email}</p>
+    `;
+    userCard.className = "border border-gray-300 rounded-lg shadow-md py-8 px-5";
+    allUserData.append(userCard);
+  });
+}
 
 async function fetchUsers() {
   loading.style.display = "block";
 
-  try {
+  try{
     const response = await fetch(url);
-    const users = await response.json();
-
-    users.forEach((userData)=>{
-      let newUser = document.createElement('div');
-      newUser.className = "p-5 rounded-lg border border-gray-100 shadow-lg shadow-gray-300";
-      newUser.innerHTML = `
-        <h2 class="text-xl text-gray-900 font-bold">${userData.name}</h2>
-        <p class="text-md text-gray-600 mt-3 ">${userData.email}</p>
-      `;
-      allUserData.append(newUser);
-    });
+    masterList = await response.json();
+    renderUsers(masterList);
   } catch {
-    let error = document.createElement('div');
-    error.textContent = 'Failed to load';
-    allUserData.append(error);
+    allUserData.append(`Failed to Load`);
   } finally {
     loading.style.display = "none";
-  };
-};
-
+  }
+}
 fetchUsers();

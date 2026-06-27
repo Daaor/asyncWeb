c4ce5c0eb65a7f22a5488de2bd8310dc077a7fcf
Daaor <praisedaaor@gmail.com>
2026-06-27 19:35:53 +0100

    added delete functionality to the cards

--- src/main.js
+++ src/main.js
@@ -19,8 +19,10 @@ function renderUsers(usersList){ // Handles creating of individual cards from th
     userCard.innerHTML = `
       <h2 class="text-xl font-bold text-center">${user.name}</h2>
       <p class="text-lg text-center pt-5">${user.email}</p>
+      <button class="deleteBtn px-4 py-2 rounded-full bg-red-300 border-none text-white hover:bg-red-600 hover:shadow-lg hover:-translate-y-1 transition-all duration-500 ease-in-out">Delete user</button>
     `;
-    userCard.className = "border border-gray-300 rounded-lg shadow-md py-8 px-5";
+    userCard.className = "js-data-card border border-gray-300 rounded-lg shadow-md py-8 px-5";
+    userCard.dataset.id = user.id;
     allUserData.append(userCard);
   });
 }
@@ -30,12 +32,25 @@ async function fetchUsers() {
 
   try{
     const response = await fetch(url);
-    masterList = await response.json();
+    masterList = await response.json(); // Where the json file was assigned to masterList.
     renderUsers(masterList);
   } catch {
     allUserData.append(`Failed to Load`);
   } finally {
     loading.style.display = "none";
-  }
+  };
 }
+
+allUserData.addEventListener("click", (e) => {  //for deletion
+  if (e.target.classList.contains('deleteBtn')) { // If the list of classes associated with the target element contains the class 'deleteBtn'...execute.
+    const cardContainer = e.target.closest('.js-data-card'); // This gets the element closest to the valid target element with the class 'js-data-card'.
+    const userToDelete = Number(cardContainer.dataset.id); // converts the id grabbed from the json file to a number.
+
+    masterList = masterList.filter((user)=>{
+      return user.id !== userToDelete;
+    })
+    renderUsers(masterList);
+  }
+});
+
 fetchUsers();
